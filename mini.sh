50a64cab95538dace8d1de29afa12cc8d0de733c
Felix <github@fe.felaheim.com>
2026-07-15 22:48:59 +0200

    Update input JSON file and adjust num_frames in demo script

--- mini.sh
+++ mini.sh
@@ -4,11 +4,11 @@ torchrun --nproc_per_node=1 run_demo_avatar_single_audio_to_video.py \
   --context_parallel_size=1 \
   --checkpoint_dir=./weights/LongCat-Video-Avatar-1.5 \
   --stage_1=ai2v \
-  --input_json=assets/avatar/single_example_1.json \
+  --input_json=assets/avatar/single_example_elena.json \
   --use_distill \
   --model_type=avatar-v1.5 \
   --use_int8 \
-  --num_frames 93
-#   --num_frames 49
+  --num_frames 49
+#   --num_frames 93
 
 
